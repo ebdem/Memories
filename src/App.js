6804a8efdce233d701112c66116740e1991658ae
ebdem <ebu.bekir.3333@gmail.com>
2020-11-29 21:59:50 +0300

    its done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from "react";
+import React,{useEffect,useState} from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import memories from "./images/memories.png";
 
@@ -18,6 +18,8 @@ import { getPosts } from "./actions/posts";
 
 export default function App(){
 
+    const [currentId, setCurrentId] = useState(null);
+
     const classes = useStyles();
     const dispatch = useDispatch();
 
@@ -35,10 +37,10 @@ export default function App(){
                 <Container>
                     <Grid container justify="space-between" alignItems="stretch" spacing={3}>
                         <Grid item xs={12} sm={7}>
-                            <Posts/>
+                            <Posts setCurrentId={setCurrentId} />
                         </Grid>
                         <Grid item xs={12} sm={4}>
-                            <Form/>
+                            <Form currentId={currentId} setCurrentId={setCurrentId} />
                         </Grid>
                     </Grid>
                 </Container>
